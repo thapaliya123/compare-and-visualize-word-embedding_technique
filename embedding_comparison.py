c7201f618bdd3c22c94b5a588c9d17a01312a9e2
anish_thapaliya <athapaliya@pushnami.com>
2023-02-16 23:35:26 +0545

    add dir struct

---
+++ embedding_comparison.py
@@ -0,0 +1,4 @@
+import numpy as np
+from gensim.models import Word2Vec, KeyedVectors
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from transformers import BertTokenizer, BertModel
