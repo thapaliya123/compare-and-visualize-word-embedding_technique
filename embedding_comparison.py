1d16cc3a2b70aa167612f4f2401004e58a72af47
anish_thapaliya <athapaliya@pushnami.com>
2023-02-18 23:36:14 +0545

    add constructor

--- embedding_comparison.py
+++ embedding_comparison.py
@@ -2,3 +2,17 @@ import numpy as np
 from gensim.models import Word2Vec, KeyedVectors
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from transformers import BertTokenizer, BertModel
+
+class EmbeddingComparison:
+    def __init__(self, corpus) -> None:
+        self.corpus = corpus
+        self.words = ['apple', 'orange', 'banana', 'car', 'bus', 'train']
+        self.embeddings = {
+            'Word2Vec': None,
+            'FastText': None,
+            'Glove': None,
+            'BOW': None,
+            'TF-IDF': None,
+            'BERT': None
+        }
+        self.word_vectors = None
