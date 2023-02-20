87fb1ec66e006ab925dfa23919b88070a33b51cd
anish_thapaliya <athapaliya@pushnami.com>
2023-02-20 23:37:21 +0545

    add module for classical embeddings

--- embedding_comparison.py
+++ embedding_comparison.py
@@ -1,5 +1,5 @@
 import numpy as np
-from gensim.models import Word2Vec, KeyedVectors
+from gensim.models import Word2Vec, FastText, KeyedVectors
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from transformers import BertTokenizer, BertModel
 
@@ -16,3 +16,19 @@ class EmbeddingComparison:
             'BERT': None
         }
         self.word_vectors = None
+
+
+    def train_classical_word_embedding(self, model_type, **kwargs):
+        if model_type == 'Word2Vec':
+            self.embeddings[model_type] = Word2Vec(self.corpus, **kwargs)
+        
+        elif model_type == 'FastText':
+            self.embeddings[model_type] = FastText(self.corpus, **kwargs)
+        
+        elif model_type == 'Glove':
+            self.embeddings[model_type] = KeyedVectors.load_word2vec_format(kwargs['file_path'], binary=False)
+
+        else:
+            raise ValueError(f'Unknown embedding type: {model_type}')
+        
+        return model_type
