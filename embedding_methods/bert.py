bcd175a9ff033566247f3c9b62400eba18781bf6
thapaliya123 <thapaliyanish123@gmail.com>
2023-03-14 15:26:10 +0545

    [UPDATE] Bert bug fix

--- embedding_methods/bert.py
+++ embedding_methods/bert.py
@@ -1,5 +1,7 @@
+import numpy as np
 import torch
 from transformers import BertTokenizer, BertModel
+from embedding_methods.visualize_embeddings import VisualizeEmbeddings
 
 class BertEmbeddings(VisualizeEmbeddings):
     '''
@@ -7,10 +9,9 @@ class BertEmbeddings(VisualizeEmbeddings):
     '''
 
     def __init__(self, bert_model_name: str, plot_save_dir: str) -> None:
-        super().__init__(model_name='bert')
+        super().__init__(model_name='bert', plot_save_dir=plot_save_dir)
         self.bert_model_name = bert_model_name
         
-
     def load_model(self):
         '''
         Load pretrained BertModel and BertTokenizer
@@ -20,13 +21,40 @@ class BertEmbeddings(VisualizeEmbeddings):
         '''
         print('Loading pretrained BERT model and BERT tokenizers!!!')
         self.bert_model = BertModel.from_pretrained(self.bert_model_name)
-        self.bert_tokenizer = BertModel.from_pretrained(self.bert_model_name)
+        self.bert_tokenizer = BertTokenizer.from_pretrained(self.bert_model_name)
 
     def forward_propagate(self, word, context):
         encoded_context = self.bert_tokenizer.encode_plus(context, add_special_tokens=True, return_tensors='pt')
         with torch.no_grad():
             outputs = self.bert_model(encoded_context['input_ids'], attention_mask=encoded_context['attention_mask'])
-            outputs.last_hidden_state[0][encoded_context['input_ids'].numpy().tolist()[0].index(self.bert_tokenizer.vocab[word])].numpy().reshape(768,)
+            outputs = outputs.last_hidden_state[0][encoded_context['input_ids'].numpy().tolist()[0].index(self.bert_tokenizer.vocab[word])].numpy().reshape(768,)
+            
+        return outputs
+
+    def __call__(self, word_context_pair):
+        # load pretrained bert model
+        # including bert_tokenizer, bert_model
+        self.load_model()
+
+        # forward propagate
+        # get embeddings
+        word_vector_arr = np.array([self.forward_propagate(word, context) for word, context in word_context_pair])
+
+        # apply pca
+        # visualize word vectors
+        self.visualize_word_embeddings(word_vector_arr, word_context_pair)
+
+        return word_vector_arr
+    
+
+def main():
+    word_context_pair = [('bank', 'river bank'), ('bank', 'bank account'), ('deposit', 'bank deposit'), ('withdrawal', 'bank withdrawal')]
+    plot_save_dir = '/home/fm-pc-lt-125/Documents/personal/compare-and-visualize-word-embedding_technique/static/plots/'
+    bert = BertEmbeddings('bert-base-uncased', plot_save_dir)
+    print('Generating Bert Embeddings Vector!!!')
+    bert(word_context_pair)
+if __name__ == '__main__':
+    main()
 
         
 
--- main.py
+++ main.py
@@ -1,25 +1,30 @@
 import os
 import setup
-from embedding_methods import word2vec, glove
+from embedding_methods import word2vec, glove, bert
 # get environment variables
 pretrained_word2vec = os.getenv('pretrained_word2vec')
 pretrained_glove = os.getenv('pretrained_glove')
+pretrained_bert = os.getenv('pretrained_bert')
 plot_save_dir = os.getenv('plot_save_dir')
 
 WORD_CONTEXT_PAIR = [('bank', 'river bank'), ('bank', 'bank account'), ('deposit', 'bank deposit'),\
                       ('withdrawal', 'bank withdrawal')]
 
+
 # Instantiate the Word EmbeddingComparison class and compare the embeddings
 word2vec = word2vec.Word2Vec(pretrained_word2vec, plot_save_dir)
 glove = glove.Glove(pretrained_glove, plot_save_dir)
+bert = bert.BertEmbeddings(pretrained_bert, plot_save_dir)
 
 def main():
-    words, contexts = zip(*WORD_CONTEXT_PAIR)
-    print('\n**Word2Vec Embeddings**')
+    print('\n**Word2Vec EMBEDDINGS**')
     word2vec(WORD_CONTEXT_PAIR)
 
     print('\n**GLOVE EMBEDDINGS**')
     glove(WORD_CONTEXT_PAIR)
+
+    print('\n**BERT EMBEDDINGS**')
+    bert(WORD_CONTEXT_PAIR)
 
 if __name__=='__main__':
     main()
